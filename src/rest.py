839d35b22e04e140823f6e8d865322444a283f30
nelson-bht <s81071@beuth-hochschule.de>
2020-12-23 20:57:42 +0100

    update

--- src/rest.py
+++ src/rest.py
@@ -5,35 +5,36 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 questions = [{'text': 'What equals 4x4?',
-		'answer': '16'}]
+        'answer': '16'}]
 
 # Handle PUT and GET for all questions
 @app.route('/Questions', methods=['PUT', 'GET'])
 def get_put_question():
-	if request.method=='GET':
-		return jsonify(questions)
-	if request.method=='PUT':
-		text = request.json['text']
-		answer = request.json['answer']
-		questions.append({'text': text,
-				'answer': answer})
-		return questions
+    if request.method=='GET':
+        return jsonify(questions)
+    if request.method=='PUT':
+        print(request)
+        # text = request.json['text']
+        # answer = request.json['answer']
+        # questions.append({'text': text,
+        #         'answer': answer})
+        return questions
 
 # Handle POST and GET for entry
 @app.route('/Questions/<int:id>', methods=['POST', 'GET'])
 def get_post_question_entry(id):
-	if id>len(questions)-1: # return last entry if id is out of range
-		id = len(questions)-1
-	if request.method=='GET':
-		return jsonify(questions[id])
-	if request.method=='POST':
-		text = request.json['text']
-		answer = request.json['answer']
-		questions[id] = {'text': text,'answer': answer}
-		return jsonify(questions[id])
+    if id>len(questions)-1: # return last entry if id is out of range
+        id = len(questions)-1
+    if request.method=='GET':
+        return jsonify(questions[id])
+    if request.method=='POST':
+        text = request.json['text']
+        answer = request.json['answer']
+        questions[id] = {'text': text,'answer': answer}
+        return jsonify(questions[id])
 
 if __name__=='__main__':
-	app.run(port=8888)
+    app.run(port=8888)
 
 # curl -X PUT -H "Content-Type: application/json" -d '{"text": "How long is a day in hours", "answer": "24"}' localhost:8888/Questions
 # curl localhost:8888/Questions/0
--- src/user.py
+++ src/user.py
@@ -12,19 +12,30 @@ def get_question_entry(id: int):
 	resp = requests.get(f'http://localhost:8888/Questions/{id}')
 	return resp.json()
 
+def add_question(text: str, answer: str):
+    # PUT request to add questions
+    resp = requests.put('http://localhost:8888/Questions', data={'text': text,
+                                                                'answer': answer})
+    return resp.json()
+
 def main():
-	while True:
-		inp = input('1: get_questions()\n'+
-				'2: get_question_entry(id)\n'+
-				'STOP: Close program\n')
-		if inp == '1':
-			print(get_questions())
-		if inp == '2':
-			id = int(input('Enter id for element starting by 0: '))
-			print(get_question_entry(id))
-		if inp == 'STOP' or inp == 'stop':
-			break
-	print('closing...')
+    while True:
+        inp = input('1: get_questions()\n'+
+                '2: get_question_entry(id)\n'+
+                '3: add_question(text, answer\n)'+
+                'STOP: Close program\n')
+        if inp == '1':
+            print(get_questions())
+        if inp == '2':
+            id = int(input('Enter id for element starting by 0: '))
+            print(get_question_entry(id))
+        if inp == '3':
+            text = input('Enter text: ')
+            answer = input('Enter answer: ')
+            print(add_question(text, answer))
+        if inp == 'STOP' or inp == 'stop':
+            break
+    print('closing...')
 
 if __name__=='__main__':
 	main()
